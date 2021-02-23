8ea3148c682e4f8700dd823c477283ab93d1e4c5
tokumorisky <e205717@ie.u-ryukyu.ac.jp>
2021-02-24 02:23:24 +0900

    1st commit

---
+++ report5.java
@@ -0,0 +1,11 @@
+public class report5{
+    static String str = null;
+
+    public static int len(){
+        return str.length();
+    }
+    public static void main(String[] args) {
+        System.out.println(len());
+    }
+
+}
